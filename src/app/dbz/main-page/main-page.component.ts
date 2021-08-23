4a3b0bcda13c8bd969addf748d37c7d45ce72b1a
jreyarijon <reyarijonjose@gmail.com>
2021-08-23 19:15:13 +0200

    directivas de atributo en el form

--- src/app/dbz/main-page/main-page.component.ts
+++ src/app/dbz/main-page/main-page.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+
+interface Personaje{
+  nombre: string;
+  poder: number;
+}
 
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
   styleUrls: ['./main-page.component.css']
 })
-export class MainPageComponent implements OnInit {
 
-  constructor() { }
+export class MainPageComponent{
 
-  ngOnInit(): void {
+  nuevo: Personaje = {
+    nombre: 'Trunks',
+    poder: 14000
+  }
+
+  cambiarNombre( event:any ){
+    console.log(event.target.value);
+  }
+
+  agregar(){
+    console.log(this.nuevo);
   }
 
 }
